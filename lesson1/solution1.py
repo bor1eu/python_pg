8face97e480a9051ec4081079f28ac70658a31c4
bor1eu <bor1eu@mail.ru>
2020-12-03 22:03:19 +0300

    test

--- lesson1/solution1.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-digit_string = sys.argv[1]
-
-result=0
-for i in digit_string:
-    result += int(i)
-print(result)
--- lesson1/solution2.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-num = int(sys.argv[1])
-
-for i in range(1,num+1):
-    print(' '*(num - i) + '#'*i)
