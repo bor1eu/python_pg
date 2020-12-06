6ebe5ca488185bb19e8e4b4c6b0b008051f83555
bor1eu <bor1eu@mail.ru>
2020-12-06 22:10:50 +0300

    lesson4 start

---
+++ lesson4/solution.py
@@ -0,0 +1,15 @@
+class File:
+    def __init__(self):
+        pass
+
+    def read(self):
+        pass
+
+    def write(self):
+        pass
+
+    def __add__(self):
+        pass
+
+    def __getitem__(self):
+        pass
